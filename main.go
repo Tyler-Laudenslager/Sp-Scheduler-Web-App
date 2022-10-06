7d60fa2ce56e0e6e05c9a4e2076f494b2e43bfa4
Tyler L <tyler@lauden.cloud>
2022-10-06 10:01:32 -0400

    Initial Commit

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello, World!")
+}
+func main() {
+	server := http.Server{
+		Addr: "127.0.0.1:6600",
+	}
+	http.HandleFunc("/", index)
+	server.ListenAndServe()
+}
