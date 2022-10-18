99506ef195a2ccb22f6bdabf3326432061cf3350
Tyler L <tyler@lauden.cloud>
2022-10-18 08:28:17 -0400

    Added SQL script to drop and create the database

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"net/http"
-	"text/template"
 
 	"github.com/gorilla/sessions"
 )
@@ -14,101 +12,6 @@ var (
 	key   = []byte("super-secret-key")
 	store = sessions.NewCookieStore(key)
 )
-
-func login(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "sessionAuthSPCalendar")
-	if auth, ok := session.Values["authenticated"].(bool); ok && auth {
-		http.Redirect(w, r, "/dashboard", http.StatusFound)
-	}
-	t, _ := template.ParseFiles("html-boilerplate.html", "login-content.html")
-	t.ExecuteTemplate(w, "html-boilerplate", "")
-}
-
-func dashboard(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "sessionAuthSPCalendar")
-	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-		http.Redirect(w, r, "/login", http.StatusFound)
-		return
-	}
-	spuser, err := GetSpUserRecord(session.Values["username"].(string), db)
-
-	if err != nil {
-		fmt.Println("Error Get SP user record in authenticate: ", err)
-		return
-	}
-	/* fmt.Fprintln(w, "User Authenticated with Cookie!")
-	fmt.Fprintln(w, "Welcome to the Dashboard ", spuser.Name.First, spuser.Name.Last) */
-	t, _ := template.ParseFiles("html-boilerplate.html", "dashboard-content.html", "session-content.html")
-	session_records, err := GetAllSessionInfoRecords(db)
-	if err != nil {
-		fmt.Println("Error Get All Session Records: ", err)
-	}
-
-	spuser.SessionsAvailable = session_records
-
-	t.ExecuteTemplate(w, "html-boilerplate", spuser)
-}
-
-func authenticate(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "sessionAuthSPCalendar")
-	username := r.PostFormValue("userid")
-	password := r.PostFormValue("password")
-	spuser, err := GetSpUserRecord(username, db)
-	if err != nil {
-		http.Redirect(w, r, "/login", http.StatusFound)
-	}
-	if !CheckPasswordHash(password, spuser.Password) {
-		http.Redirect(w, r, "/login", http.StatusFound)
-	} else {
-		session.Values["authenticated"] = true
-		session.Values["username"] = spuser.Username
-	}
-	if auth, ok := session.Values["authenticated"].(bool); !ok || !auth {
-		http.Redirect(w, r, "/login", http.StatusFound)
-	}
-	session.Save(r, w)
-	http.Redirect(w, r, "/dashboard", http.StatusFound)
-}
-
-func sendjson(w http.ResponseWriter, r *http.Request) {
-	bob_marcs := SpUser{}.Create(*Name{}.Create("Bob Marcs"), SP, Male, "bob@marcs.com")
-	susan_miller := SpUser{}.Create(*Name{}.Create("Susan Miller"), SP, Female, "susan@miller.com")
-
-	andy_thomas := SpManager{}.Create(*Name{}.Create("Andy Thomas"), Manager, "andy@thomas.com")
-
-	session1 := Session{}.Create("11/15/2022", "11:00AM", "1H", "Anderson", "Check-Up")
-
-	andy_thomas.AssignedPatients = append(andy_thomas.AssignedPatients, bob_marcs, susan_miller)
-	andy_thomas.SessionsManaged = append(andy_thomas.SessionsManaged, session1)
-
-	bob_marcs.SessionsAvailable = append(bob_marcs.SessionsAvailable, session1.Information)
-	bob_marcs.SessionsAssigned = append(bob_marcs.SessionsAssigned, session1.Information)
-
-	susan_miller.SessionsAvailable = append(susan_miller.SessionsAvailable, session1.Information)
-	susan_miller.SessionsAssigned = append(susan_miller.SessionsAssigned, session1.Information)
-
-	session1.PatientsAvailable = append(session1.PatientsAvailable, bob_marcs)
-	session1.PatientsAvailable = append(session1.PatientsAvailable, susan_miller)
-	session1.PatientsAssigned = append(session1.PatientsAssigned, bob_marcs)
-	session1.PatientsAssigned = append(session1.PatientsAssigned, susan_miller)
-
-	SpUsersBox := append(make(SpUsersBox, 0, 2), bob_marcs, susan_miller)
-	SpManagersBox := append(make(SpManagersBox, 0, 1), andy_thomas)
-	SessionsBox := append(make(SpSessionsBox, 0, 1), session1)
-
-	HospitalCalendar := HospitalCalendar{
-		Users:    SpUsersBox,
-		Managers: SpManagersBox,
-		Sessions: SessionsBox,
-	}
-
-	output, err := json.MarshalIndent(HospitalCalendar, "", "\t\t")
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.Write(output)
-}
 
 func main() {
 	server := http.Server{
---
+++ sp_calendar_db_setup.sql
@@ -0,0 +1,44 @@
+DROP TABLE SpUsers;
+DROP TABLE SpManagers;
+DROP TABLE Sessions;
+
+
+CREATE TABLE SpUsers (
+	id SERIAL PRIMARY KEY,
+	name JSONB NOT NULL,
+	username varchar(30),
+	role INT4 NOT NULL,
+	sex  INT4 NOT NULL, 
+	sessionsavailable JSONB,
+	sessionsunavailable JSONB,
+	sessionsassigned JSONB,
+	password varchar(100),
+	email varchar(50) NOT NULL
+);
+
+CREATE TABLE SpManagers (
+	id SERIAL PRIMARY KEY,
+	name JSONB NOT NULL,
+	username varchar(30) NOT NULL,
+	role INT NOT NULL,
+	password varchar(50),
+	email varchar(50) NOT NULL,
+	assignedpatients JSONB,
+	sessionsmanaged JSONB,
+	sessionsunmanaged JSONB
+);
+
+CREATE TABLE Sessions (
+	id SERIAL PRIMARY KEY,
+ 	date varchar(50) NOT NULL,
+	time varchar(50) NOT NULL,
+	duration varchar(50) NOT NULL,
+	location varchar(50) NOT NULL,
+	description varchar(300),
+	Instructors JSONB,
+	PatientsNeeded int4 NOT NULL,
+	PatientsAvailable JSONB,
+	PatientsAssigned JSONB,
+	PatientsUnavailable JSONB,
+	PatientsNoResponse JSONB
+);
